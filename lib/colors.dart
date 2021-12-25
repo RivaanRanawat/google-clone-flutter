52a10f5d7f31b76380fd34948c1694b0a8f1e4ca
Rivaan Ranawat <namanrivaan@gmail.com>
2021-12-25 09:34:00 +0530

    adding images and colors

---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color.fromRGBO(32, 33, 36, 1);
+const searchColor = Color.fromRGBO(48, 49, 52, 1);
+const blueColor = Color.fromRGBO(138, 180, 248, 1);
+const searchBorder = Color.fromRGBO(95, 99, 104, 1);
